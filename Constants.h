110600334f0789b1659b7d8161a072ce05868dec
Enomi-4mg <miyashu2741@gmail.com>
2026-01-07 20:35:01 +0900

    feat : 2026-01-07 2-phase1

--- Constants.h
+++ Constants.h
@@ -2,20 +2,16 @@
 #pragma once
 #include "ofMain.h"
 
-// 成長タイプの定義
-enum GrowthType {
-    TYPE_DEFAULT,
-    TYPE_ELEGANT,  // 長さ重視
-    TYPE_STURDY,   // 太さ重視
-    TYPE_ELDRITCH  // カオス重視
-};
+enum GrowthType { TYPE_DEFAULT, TYPE_ELEGANT, TYPE_STURDY, TYPE_ELDRITCH };
+enum FlowerType { FLOWER_NONE, FLOWER_CRYSTAL, FLOWER_PETAL, FLOWER_SPIRIT };
+enum CommandType { CMD_WATER, CMD_FERTILIZER, CMD_KOTODAMA };
 
-// 開花タイプの定義
-enum FlowerType {
-    FLOWER_NONE,
-    FLOWER_CRYSTAL, // 雪の結晶
-    FLOWER_PETAL,   // 花弁
-    FLOWER_SPIRIT   // 霊魂（ヒトダマ）
+struct UISettings {
+    string labelWater, labelFertilizer, labelKotodama;
+    float btnW, btnH, btnMargin, btnBottomOffset;
+    ofColor colIdle, colHover, colActive, colLocked, colText;
+    float cooldownDuration;
+    float statusTop, statusRight;
 };
 
 struct TreeSettings {
@@ -39,5 +35,7 @@ struct GameState {
     int maxDays = 50;
     GrowthType currentType = TYPE_DEFAULT;
     FlowerType currentFlowerType = FLOWER_NONE;
-    int resilienceLevel = 0;
+    int resilienceLevel = 0; 
+    float actionCooldown = 0.0f;
+    UISettings ui;
 };
